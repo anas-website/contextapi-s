3f5801f40c7e7927d7ec0c12ab41ead4818366f6
anas <anas3506606@gmail.com>
2020-07-10 03:25:58 +0300

    Updates

---
+++ precache-manifest.8e505e0ae2adf39060532f4ba1a27d7a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c9f7654c6cb76d5ee4e661b27f5472f",
+    "url": "/contextapi-s/index.html"
+  },
+  {
+    "revision": "697885aa926b6d221d62",
+    "url": "/contextapi-s/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7ff3d730c8dd2a013300",
+    "url": "/contextapi-s/static/css/main.cb807a95.chunk.css"
+  },
+  {
+    "revision": "697885aa926b6d221d62",
+    "url": "/contextapi-s/static/js/2.562aaa82.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/contextapi-s/static/js/2.562aaa82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ff3d730c8dd2a013300",
+    "url": "/contextapi-s/static/js/main.f0a3bd3d.chunk.js"
+  },
+  {
+    "revision": "6133598dc1c6e6e315be",
+    "url": "/contextapi-s/static/js/runtime-main.56708701.js"
+  },
+  {
+    "revision": "f479f8c22875a98e3175af37f8f75d12",
+    "url": "/contextapi-s/static/media/hose.f479f8c2.png"
+  }
+]);
